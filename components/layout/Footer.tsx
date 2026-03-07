c3d5d114ad99c4f5169b1fca443aa57da2e6dc6b
noahdevelopsio <noahbig001@gmail.com>
2026-03-07 22:11:10 +0100

    fix(ts): add proper type definition for Footer social links to resolve ESLint any error

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -65,7 +65,7 @@ export default function Footer() {
                 {col.title}
               </h5>
               <ul className="space-y-3">
-                {col.links.map((link: any) => (
+                {col.links.map((link: { label: string; href: string; icon?: React.ElementType }) => (
                   <li key={link.label}>
                     <Link
                       href={link.href}
